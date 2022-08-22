9645057381f85ce866e914065c63fc60d83c58c5
dattv2723 <dattv2@rikkeisoft.com>
2022-08-22 23:20:56 +0700

    Section 8: Cocktails API Project

---
+++ Base Projects/15-cocktails/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+        facilis, minima, sequi quae dolorem voluptatem fugiat delectus optio
+        libero eaque pariatur repellat porro perferendis animi nihil hic qui
+        natus assumenda consequatur error facere nulla iste! Laboriosam ratione
+        minima doloremque ipsam quae? Tempora commodi necessitatibus eligendi
+        explicabo officia, autem sequi deserunt?
+      </p>
+    </section>
+  )
+}
+
+export default About
