0b2fb0da80741a7bb67a74be7c5abccc79bcc3f3
dattv2723 <dattv2@rikkeisoft.com>
2022-09-29 22:58:05 +0700

    Section 19: Jobster Project - Profile page

--- Jobster/src/App.js
+++ Jobster/src/App.js
@@ -7,6 +7,8 @@ import {
   AddJob,
   SharedLayout,
 } from './pages/dashboard'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -29,6 +31,7 @@ function App() {
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   )
 }
--- Jobster/src/features/user/userSlice.js
+++ Jobster/src/features/user/userSlice.js
@@ -6,6 +6,11 @@ import {
   removeUserFromLocalStorage,
 } from '../../utils/localStorage.js'
 import customFetch from '../../utils/axios.js'
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from './userThunk.js'
 
 const initialState = {
   isLoading: false,
@@ -16,24 +21,21 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/register', user)
-      return resp.data
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg)
-    }
+    return registerUserThunk('/auth/registerUser', user, thunkAPI)
   }
 )
 
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/login', user)
-      return resp.data
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg)
-    }
+    return loginUserThunk('/auth/loginUser', user, thunkAPI)
+  }
+)
+
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (user, thunkAPI) => {
+    return updateUserThunk('/auth/updateUser', user, thunkAPI)
   }
 )
 
@@ -77,6 +79,21 @@ const userSlice = createSlice({
       toast.success(`Welcome Back ${user.name}`)
     },
     [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [updateUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      addUserToLocalStorage(user)
+
+      toast.success('User Updated')
+    },
+    [updateUser.rejected]: (state, { payload }) => {
       state.isLoading = false
       toast.error(payload)
     },
